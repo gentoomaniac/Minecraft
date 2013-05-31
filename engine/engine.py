e866a5de588a0220bf6f5d0fb1c9125fa2ee0951
Marco Siebecke <marco@spotify.com>
2013-06-01 00:23:04 +0200

    crouch working

--- engine/engine.py
+++ engine/engine.py
@@ -9,6 +9,8 @@ from pyglet.gl import *
 from pyglet.window import key, mouse
 from pyglet.gl import *
 
+from transform import *
+
 TEXTURE_PATH = 'texture.png'
 
 
@@ -28,81 +30,6 @@ SECTOR_SIZE = 16
 
 TERMINAL_VELOCITY = 50
 
-
-def cube_vertices(x, y, z, n):
-    """ Return the vertices of the cube at position x, y, z with size 2*n.
-
-    """
-    return [
-        x-n,y+n,z-n, x-n,y+n,z+n, x+n,y+n,z+n, x+n,y+n,z-n,  # top
-        x-n,y-n,z-n, x+n,y-n,z-n, x+n,y-n,z+n, x-n,y-n,z+n,  # bottom
-        x-n,y-n,z-n, x-n,y-n,z+n, x-n,y+n,z+n, x-n,y+n,z-n,  # left
-        x+n,y-n,z+n, x+n,y-n,z-n, x+n,y+n,z-n, x+n,y+n,z+n,  # right
-        x-n,y-n,z+n, x+n,y-n,z+n, x+n,y+n,z+n, x-n,y+n,z+n,  # front
-        x+n,y-n,z-n, x-n,y-n,z-n, x-n,y+n,z-n, x+n,y+n,z-n,  # back
-    ]
-
-
-def tex_coord(x, y, n=4):
-    """ Return the bounding vertices of the texture square.
-
-    """
-    m = 1.0 / n
-    dx = x * m
-    dy = y * m
-    return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
-
-
-def tex_coords(top, bottom, side):
-    """ Return a list of the texture squares for the top, bottom and side.
-
-    """
-    top = tex_coord(*top)
-    bottom = tex_coord(*bottom)
-    side = tex_coord(*side)
-    result = []
-    result.extend(top)
-    result.extend(bottom)
-    result.extend(side * 4)
-    return result
-
-
-
-
-
-def normalize(position):
-    """ Accepts `position` of arbitrary precision and returns the block
-    containing that position.
-
-    Parameters
-    ----------
-    position : tuple of len 3
-
-    Returns
-    -------
-    block_position : tuple of ints of len 3
-
-    """
-    x, y, z = position
-    x, y, z = (int(round(x)), int(round(y)), int(round(z)))
-    return (x, y, z)
-
-
-def sectorize(position):
-    """ Returns a tuple representing the sector for the given `position`.
-
-    Parameters
-    ----------
-    position : tuple of len 3
-
-    Returns
-    -------
-    sector : tuple of len 3
-
-    """
-    x, y, z = normalize(position)
-    x, y, z = x / SECTOR_SIZE, y / SECTOR_SIZE, z / SECTOR_SIZE
-    return (x, 0, z)
 
 
 class Model(object):
@@ -411,7 +338,7 @@ class Model(object):
 
 
 
-GRASS = tex_coords((1, 0), (0, 1), (0, 0))
+GRASS = Tools.tex_coords((1, 0), (0, 1), (0, 0))
 SAND = tex_coords((1, 1), (1, 1), (1, 1))
 BRICK = tex_coords((2, 0), (2, 0), (2, 0))
 STONE = tex_coords((2, 1), (2, 1), (2, 1))
@@ -752,6 +679,9 @@ class Core(pyglet.window.Window):
             self.strafe[1] += 1
         elif symbol == key.C:
             self.settings['isCrouch'] = True
+            pos = list(self.position)
+            pos[1] -= self.settings['playerHight'] - self.settings['crouchHight']
+            self.position = tuple(pos)
         elif symbol == key.SPACE:
             if self.dy == 0:
                 self.dy = self.settings['jumpSpeed']
@@ -785,6 +715,9 @@ class Core(pyglet.window.Window):
             self.strafe[1] -= 1
         elif symbol == key.C:
             self.settings['isCrouch'] = False
+            pos = list(self.position)
+            pos[1] += self.settings['playerHight'] - self.settings['crouchHight']
+            self.position = tuple(pos)
 
     def on_resize(self, width, height):
         """ Called when the window is resized to a new `width` and `height`.
---
+++ engine/transform.py
@@ -0,0 +1,82 @@
+
+from collections import deque
+from pyglet import image
+from pyglet.graphics import TextureGroup
+from pyglet.gl import *
+from pyglet.window import key, mouse
+from pyglet.gl import *
+
+class Tools:
+
+    @staticmethod
+    def cube_vertices(x, y, z, n):
+        """ Return the vertices of the cube at position x, y, z with size 2*n.
+
+        """
+        return [
+            x-n,y+n,z-n, x-n,y+n,z+n, x+n,y+n,z+n, x+n,y+n,z-n,  # top
+            x-n,y-n,z-n, x+n,y-n,z-n, x+n,y-n,z+n, x-n,y-n,z+n,  # bottom
+            x-n,y-n,z-n, x-n,y-n,z+n, x-n,y+n,z+n, x-n,y+n,z-n,  # left
+            x+n,y-n,z+n, x+n,y-n,z-n, x+n,y+n,z-n, x+n,y+n,z+n,  # right
+            x-n,y-n,z+n, x+n,y-n,z+n, x+n,y+n,z+n, x-n,y+n,z+n,  # front
+            x+n,y-n,z-n, x-n,y-n,z-n, x-n,y+n,z-n, x+n,y+n,z-n,  # back
+        ]
+
+    @staticmethod
+    def tex_coord(x, y, n=4):
+        """ Return the bounding vertices of the texture square.
+
+        """
+        m = 1.0 / n
+        dx = x * m
+        dy = y * m
+        return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
+
+    @staticmethod
+    def tex_coords(top, bottom, side):
+        """ Return a list of the texture squares for the top, bottom and side.
+
+        """
+        top = tex_coord(*top)
+        bottom = tex_coord(*bottom)
+        side = tex_coord(*side)
+        result = []
+        result.extend(top)
+        result.extend(bottom)
+        result.extend(side * 4)
+        return result
+
+    @staticmethod
+    def normalize(position):
+        """ Accepts `position` of arbitrary precision and returns the block
+        containing that position.
+
+        Parameters
+        ----------
+        position : tuple of len 3
+
+        Returns
+        -------
+        block_position : tuple of ints of len 3
+
+        """
+        x, y, z = position
+        x, y, z = (int(round(x)), int(round(y)), int(round(z)))
+        return (x, y, z)
+
+    @staticmethod
+    def sectorize(position, sectorSize):
+        """ Returns a tuple representing the sector for the given `position`.
+
+        Parameters
+        ----------
+        position : tuple of len 3
+
+        Returns
+        -------
+        sector : tuple of len 3
+
+        """
+        x, y, z = normalize(position)
+        x, y, z = x / sectorSize, y / sectorSize, z / sectorSize
+        return (x, 0, z)
